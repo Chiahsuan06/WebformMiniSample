c5efd5cebef43ea80fca533f7e864d13fe9e6173
Chiahsuan06 <chiahsuan56566498@gmail.com>
2021-07-29 15:22:59 +0800

    上船現有專案

---
+++ AccountingNote.DBSource/UserInfoManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingNote.DBSource
+{
+    public class UserInfoManager
+    {
+        public static string GetConnectionString()
+        {
+            string val = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            return val;
+        }
+        public static DataRow GetUserInfoByAccount(string account)
+        {
+            string connectionString = GetConnectionString();
+
+            string daCommandString =
+                 @"SELECT [ID], [Account], [PWD], [Name], [Email]
+                   FROM UserInfo
+                   WHERE [Account] = @account
+                 ";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(daCommandString, connection))
+                {
+                    command.Parameters.AddWithValue("@account", account);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        reader.Close();
+
+                        if (dt.Rows.Count == 0)
+                            return null;
+
+                        DataRow dr = dt.Rows[0];
+                        return dr;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        return null;
+                    }
+                }
+            }
+        } 
+     
+
+    }
+}
---
+++ AccountingNote/Login.aspx.cs
@@ -0,0 +1,55 @@
+﻿using AccountingNote.DBSource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AccountingNote
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (this.Session["UserLogin"] != null)
+            {
+                this.plcLogin.Visible = true;
+                //Response.Redirect("/SystemAdmin/UserInfo.aspx");
+            }
+        }
+
+        protected void btnLogin_Click1(object sender, EventArgs e)
+        {
+            string inp_Account = this.txtAccount.Text;
+            string inp_PWD = this.txtPWD.Text;
+
+            if (string.IsNullOrWhiteSpace(inp_Account) || string.IsNullOrWhiteSpace(inp_PWD))
+            {
+                this.ltlMsg.Text = "Account / PWD is required.";
+                return;
+            }
+
+            var dr = UserInfoManager.GetUserInfoByAccount(inp_Account);
+
+            //check null
+            if (dr == null)
+            {
+                this.ltlMsg.Text = "Account doesn't exists.";
+                return;
+            }
+
+            //check account / PWD
+            if (string.Compare(dr["Account"].ToString(), inp_Account, true) == 0 && string.Compare(dr["PWD"].ToString(), inp_PWD, false) == 0)
+            {
+                this.Session["UserLoginInfo"] = dr["Account"].ToString();
+                Response.Redirect("/SystemAdmin/UserInfo.aspx");
+            }
+            else
+            {
+                this.ltlMsg.Text = "Login fail. Place check Account / PWD.";
+                return;
+            }
+        }
+    }
+}
