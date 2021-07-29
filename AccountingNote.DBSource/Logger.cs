1545ff43c20046c304e6588fc32deea72cc2a45e
Chiahsuan06 <chiahsuan56566498@gmail.com>
2021-07-29 21:17:41 +0800

    Logger 實作完成

--- AccountingNote.DBSource/Logger.cs
+++ AccountingNote.DBSource/Logger.cs
@@ -10,7 +10,14 @@ namespace AccountingNote.DBSource
     {
         public static void WriteLog(Exception ex)
         {
-            throw ex;
+            string msg =
+                $@"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}
+                    {ex.ToString()}
+
+                ";
+            System.IO.File.AppendAllText("D:\\Logs.\\Log.log", msg);
+              
+                throw ex;
         }
     }
 }
